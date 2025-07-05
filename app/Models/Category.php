3d1054b3c4c9bc103ccff18d51c82be3e0a3b026
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-05 15:47:24 +0300

    Expand model attributes and relationships: update fillables, add constants, scopes, and helpers for Category, Order, OrderItem, Product, and User models.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use App\Models\Product;
 
 class Category extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug', 'description', 'image'];
 
     public function products()
     {
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Order;
 use App\Models\Product;
+use \Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class OrderItem extends Model
 {
     protected $fillable = ['order_id', 'product_id', 'quantity', 'price','total_price',];
-    
+
     public function product()
     {
         return $this->belongsTo(Product::class);
